05b9e2a7664ad0f7e8b515eb3383abfc2af11078
Zeng Yilun <me10zyl@qq.com>
2016-11-10 12:29:49 +0800

    change date format

--- src/com/github/setial/intellijjavadocs/utils/ParametersUtils.java
+++ src/com/github/setial/intellijjavadocs/utils/ParametersUtils.java
@@ -13,12 +13,12 @@ import java.util.Properties;
 public class ParametersUtils {
 
     public  static String getDate() {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
         return df.format(new Date());
     }
 
     public static String getDateTime() {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         return df.format(new Date());
     }
 
